5d4eb65e392fca08df765bd034fc41587ad36fa9
Kamila Choroszucha <kamila.choroszucha@gmail.com>
2019-07-24 10:06:01 +0200

    feat: styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,19 @@
 "use strict";
-fetch("https://api.github.com/users/kamilachoroszucha/repos")
+const list = document.querySelector('.list--js')
+
+fetch("https://api.github.com/users/kamilachoroszucha/repos?sort=created&direction=asc")
   .then(resp => resp.json())
   .then(resp => {
-    
-    for(const repo of resp){
-    console.log(`${repo.name}: ${repo.html_url}`)}
+    for (const repo of resp) {
+      const { name, html_url } = repo;
+      const upperName = name.toUpperCase();
+      
+      list.innerHTML += `<li class="list__item" ><div class="list__item--title">${upperName}:</div> <div class="list__item--address"><a class="list__item--link" href="${html_url}">${html_url}</a></div></li>`;
+     
+      
+    }
   })
-  .catch(err=>
+  .catch(err =>
     console.log(err))
 
 
